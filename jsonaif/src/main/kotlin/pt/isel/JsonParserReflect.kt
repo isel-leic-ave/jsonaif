4793671ad966076ed8812a8f280fdaa8ed481cc5
Miguel Gamboa <miguelgamboa@outlook.com>
2022-03-09 15:50:38 +0000

    Fix

--- jsonaif/src/main/kotlin/pt/isel/JsonParserReflect.kt
+++ jsonaif/src/main/kotlin/pt/isel/JsonParserReflect.kt
@@ -8,6 +8,7 @@ object JsonParserReflect  : AbstractJsonParser() {
      * For each domain class we keep a Map<String, Setter> relating properties names with their setters.
      */
     private val setters = mutableMapOf<KClass<*>, Map<String, Setter>>()
+    
     override fun parsePrimitive(tokens: JsonTokens, klass: KClass<*>): Any? {
         TODO("Not yet implemented")
     }
